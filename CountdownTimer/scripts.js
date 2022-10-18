073c5483d82b5478db09f01b34aed2bb61ef192c
mertcanturk <mertcanturk63@hotmail.com>
2022-10-18 22:47:49 +0300

    first commit

---
+++ CountdownTimer/scripts.js
@@ -0,0 +1,34 @@
+const newYear = '1 Jan 2023';
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minEl = document.getElementById("min");
+const secEl = document.getElementById("sec");
+
+function Countdown(){
+    const newYearsDate = new Date(newYear);
+    const currentDate = new Date();
+    const totalSeconds = (newYearsDate - currentDate)/1000;
+    
+    
+    const days = Math.floor(totalSeconds /3600/24);
+    const hours = Math.floor(totalSeconds /3600)%24;
+    const minutes = Math.floor(totalSeconds /60)%60;
+    const seconds = Math.floor(totalSeconds) %60;
+
+
+    dayEl.innerHTML = formatTime(days);
+    hourEl.innerHTML = formatTime(hours);
+    minEl.innerHTML = formatTime(minutes);
+    secEl.innerHTML = formatTime(seconds);
+
+
+}
+function formatTime(time){
+    if(time <10){
+        return `0${time}`;
+    }
+    else{
+return time;
+    }
+}
+Countdown();
